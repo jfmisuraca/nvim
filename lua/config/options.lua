0ea10fb7c82a260eab2b007d9be94d0c4808cc1a
Juan Francisco <jfmisuraca@hotmail.com>
2023-11-24 22:06:11 -0300

    reemplazada ruta del script por referencia a variable de entorno

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,7 +1,9 @@
--- Options are automatically loaded before lazy.nvim startup
--- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
--- Add any additional options here
+-- this block sources a script I use to load Spanish accents shortcuts
+local tildesVimPath = vim.fn.expand("$scripts/tildes.vim")
+if tildesVimPath and vim.fn.filereadable(tildesVimPath) == 1 then
+  vim.cmd("source " .. tildesVimPath)
+end
+
 vim.wo.relativenumber = true
 vim.opt.wrap = true
-vim.cmd("source ~/Dropbox/Aplicaciones/scripts/tildes.vim")
 vim.opt.scrolloff = 2
