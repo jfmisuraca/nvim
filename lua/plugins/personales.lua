6c99fbbfad4fef106c795c9936726c6b4938015c
Juan Francisco <jfmisuraca@hotmail.com>
2023-12-12 23:52:47 -0300

    actualizada configuracion de plugins

--- lua/plugins/personales.lua
+++ lua/plugins/personales.lua
@@ -1,6 +1,30 @@
 return {
   { "akinsho/bufferline.nvim", enabled = false },
 
+  {
+    "mickael-menu/zk-nvim",
+    config = function()
+      require("zk").setup({
+        picker = "telescope",
+
+        lsp = {
+          config = {
+            cmd = { "zk", "lsp" },
+            name = "zk",
+            -- on_attach = ...
+            -- etc, see `:h vim.lsp.start_client()`
+          },
+
+          -- automatically attach buffers in a zk notebook that match the given filetypes
+          auto_attach = {
+            enabled = true,
+            filetypes = { "markdown" },
+          },
+        },
+      })
+    end,
+  },
+
   -- add telescope-fzf-native
   {
     "telescope.nvim",
